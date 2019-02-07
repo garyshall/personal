8030b6e1e85caaee47bd95159500bde45b74b91b
Gary Shall <garyshall@163.com>
2019-02-06 19:32:22 -0800

    modify hello message to 11111111

--- src/main/java/com/study/service/PortalService.java
+++ src/main/java/com/study/service/PortalService.java
@@ -8,7 +8,7 @@ public class PortalService {
 	public String getHelloMessage() {
 		String message = "Hello";
 		message += " ~~~ ";
-		message += "$$$$$$$$";
+		message += "$11111111$";
 		return message;
 	}
 
